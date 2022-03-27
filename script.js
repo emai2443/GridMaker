70f7b7b7a2a3d5567c3f034fc73dd02ec484ee16
Eric Mai <ericmai10314@gmail.com>
2022-03-27 17:51:43 -0400

    fixed bug

--- script.js
+++ script.js
@@ -1,81 +1,85 @@
 // Declare global variables
 let numRows = 0;
 let numCols = 0;
-let colorSelected; 
-
+let colorSelected;
 
 // Add a row
 function addR() {
-    let tableRow = document.querySelector("#grid");
-    let newRow = tableRow.insertRow();
+  var numRows = grid.rows.length;
 
-    newRow.insertCell().append("");
+  if (numRows === 0) {
+    //if table is empty, add first cell
+    let row = grid.insertRow();
+    row.insertCell();
+  } else {
+    numCols = grid.rows[0].children.length;
+    let row = grid.insertRow();
+    for (let i = 0; i < numCols; i++) {
+      row.insertCell(i);
+    }
+  }
 }
 function addC() {
-    let table = document.querySelector("#grid");
-    let row = table.getElementsByTagName('tr');
-    for(i=0;i<row.length;i++){
-    row[i].innerHTML = row[i].innerHTML + '<td></td>';
-    }
+  let table = document.querySelector("#grid");
+  let row = table.getElementsByTagName("tr");
+  for (i = 0; i < row.length; i++) {
+    row[i].innerHTML = row[i].innerHTML + "<td></td>";
+  }
 }
 
 // Remove a row
 function removeR() {
-    let table = document.querySelector("#grid");
-    let row = table.getElementsByTagName('tr');
-    if(row.length!='0'){
-        row[row.length - 1].outerHTML='';
-    }
+  let table = document.querySelector("#grid");
+  let row = table.getElementsByTagName("tr");
+  if (row.length != "0") {
+    row[row.length - 1].outerHTML = "";
+  }
 }
 
 // Remove a column
 function removeC() {
-    let allRows = document.querySelector("#grid").rows;
-    for (let i=0; i<allRows.length; i++) {
-        if (allRows[i].cells.length > 0) {
-            allRows[i].deleteCell(-1);
-        }
+  let allRows = document.querySelector("#grid").rows;
+  for (let i = 0; i < allRows.length; i++) {
+    if (allRows[i].cells.length > 0) {
+      allRows[i].deleteCell(-1);
     }
+  }
 }
 
-
 // Set global variable for selected color
-function selectColor(){
-    colorSelected = document.getElementById("selectedColorId").value;
-    console.log(colorSelected);
-    let grid = document.querySelector("#grid");
-    console.log(grid);
-    grid.addEventListener("click", function onClick(event){
-        console.log(event.target);
-        event.target.style.backgroundColor = colorSelected;
-    });
+function selectColor() {
+  colorSelected = document.getElementById("selectedColorId").value;
+  console.log(colorSelected);
+  let grid = document.querySelector("#grid");
+  console.log(grid);
+  grid.addEventListener("click", function onClick(event) {
+    console.log(event.target);
+    event.target.style.backgroundColor = colorSelected;
+  });
 }
 
 // Fill all uncolored cells
-function fillU(){
-    const grid = document.querySelectorAll("table > tbody > tr > td");
-    for(let i=0; i < grid.length; i++)
-    {
-        if(!grid[i].style.backgroundColor){
-            grid[i].style.backgroundColor = colorSelected;
-
-        }
+function fillU() {
+  const grid = document.querySelectorAll("table > tbody > tr > td");
+  for (let i = 0; i < grid.length; i++) {
+    if (!grid[i].style.backgroundColor) {
+      grid[i].style.backgroundColor = colorSelected;
     }
+  }
 }
 
 // Fill all cells
-function fillAll(){
-    let rows = document.querySelectorAll("table > tbody > tr > td");
-    console.log(rows);
-    for(let i =0; i < rows.length; i++){
-        rows[i].style.backgroundColor = colorSelected;
-    }
+function fillAll() {
+  let rows = document.querySelectorAll("table > tbody > tr > td");
+  console.log(rows);
+  for (let i = 0; i < rows.length; i++) {
+    rows[i].style.backgroundColor = colorSelected;
+  }
 }
 
 // Clear all cells
-function clearAll(){
-    for(let i = document.getElementById("grid").rows.length; i > 0;i--)
-    {
-    document.getElementById("grid").deleteRow(i -1);
-    }
+function clearAll() {
+  for (let i = document.getElementById("grid").rows.length; i > 0; i--) {
+    document.getElementById("grid").deleteRow(i - 1);
+  }
 }
